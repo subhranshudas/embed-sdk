7a28d1f7fd882985c3c5921d9aaaba353d2bd866
Subhranshu Das <subhranshu@Subhranshus-MacBook-Pro.local>
2022-04-16 18:04:47 +0530

    changed app url

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export default {
-	EMBED_APP_URL: 'https://subhranshudas.github.io/embed-app',
+	EMBED_APP_URL: 'https://staging-app.epns.io/#/inbox',
 	EMBED_VIEW_ROOT: 'EPNS_SDK_EMBED_VIEW_ROOT',
 	STYLE_TAG_ID_PREFIX: 'STYLE_TAG_ID_',
 	IFRAME_ID: 'EPNS_SDK_EMBED_IFRAME',
